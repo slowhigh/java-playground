ce5b498997f14e0bbd369118ff9fcd1ce79d801e
Jo YoHan <37216082+slowhigh@users.noreply.github.com>
2025-02-26 10:04:49 +0900

    https://www.acmicpc.net/problem/1987 제출

--- Main.java
+++ Main.java
@@ -1,93 +1,81 @@
 import java.io.*;
 import java.util.*;
 
-// 5 6  // V E
-// 1    // K
-// 5 1 1
-// 1 2 2
-// 1 3 3
-// 2 3 4
-// 2 4 5
-// 3 4 6
+// 2 4
+// CAAB
+// ADCB
 
-// 5 6
-// 1
-// 5 1 1
-// 1 2 2
-// 1 3 3
-// 2 3 4
-// 2 4 5
-// 3 4 6
-
-// 0
-// 2
 // 3
-// 7
-// INF
 
 class Main {
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] ve = br.readLine().split(" ");
-        int V = Integer.parseInt(ve[0]);
-        int E = Integer.parseInt(ve[1]);
-        int K = Integer.parseInt(br.readLine());
+        String[] rc = br.readLine().split(" ");
+        int R = Integer.parseInt(rc[0]), C = Integer.parseInt(rc[1]);
 
-        Map<Integer, List<Edge>> edges = new HashMap<>();
-        for (int i = 0; i < E; i++) {
-            String[] e = br.readLine().split(" ");
-            int u = Integer.parseInt(e[0]), v = Integer.parseInt(e[1]), w = Integer.parseInt(e[2]);
-
-            if (!edges.containsKey(u))
-                edges.put(u, new ArrayList<>());
-
-            edges.get(u).add(new Edge(v, w));
-        }
-
-        int[] result = solution(V, K, edges);
-        StringBuilder sb = new StringBuilder();
-        for (int r : result) {
-            sb.append(r == Integer.MAX_VALUE ? "INF\n" : r + "\n");
-        }
-
-        System.out.println(sb.toString());
-    }
-
-    public static int[] solution(int V, int K, Map<Integer, List<Edge>> edges) {
-        int[] dist = new int[V + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[K] = 0;
-
-        PriorityQueue<Edge> pq = new PriorityQueue<>((x, y) -> x.dist - y.dist);
-        pq.add(new Edge(K, 0));
-
-        while (!pq.isEmpty()) {
-            Edge cur = pq.poll();
-
-            if (cur.dist > dist[cur.to] || !edges.containsKey(cur.to))
-                continue;
-
-            for (Edge next : edges.get(cur.to)) {
-                int newDist = cur.dist + next.dist;
-
-                if (newDist >= dist[next.to])
-                    continue;
-
-                dist[next.to] = newDist;
-                pq.add(new Edge(next.to, newDist));
+        char[][] board = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            String l = br.readLine();
+            for (int j = 0; j < C; j++) {
+                board[i][j] = l.charAt(j);
             }
         }
 
-        return Arrays.copyOfRange(dist, 1, V + 1);
+        System.out.println(solution(R, C, board));
+    }
+
+    public static final int[] DR = { -1, 1, 0, 0 }, DC = { 0, 0, -1, 1 };
+
+    public static int solution(int R, int C, char[][] board) {
+        int[][] dist = new int[R][C];
+        dist[0][0] = 1;
+        int max = 1;
+
+        Stack<Point> s = new Stack<>();
+        Point start = new Point(0, 0);
+        start.alphabet += (board[0][0]);
+        s.push(start);
+
+        while (!s.isEmpty()) {
+            Point cur = s.pop();
+
+            for (int i = 0; i < 4; i++) {
+                int r = cur.r + DR[i], c = cur.c + DC[i];
+
+                if (r < 0 || c < 0 || r >= R || c >= C)
+                    continue;
+
+                String nextStr = Character.toString(board[r][c]);
+
+                if (cur.alphabet.contains(nextStr))
+                    continue;
+
+                Point next = new Point(r, c);
+                next.alphabet += cur.alphabet + nextStr;
+                dist[r][c] = Math.max(dist[r][c], next.alphabet.length());
+                s.push(next);
+            }
+        }
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                max = Math.max(max, dist[i][j]);
+            }
+        }
+
+        return max;
     }
 }
 
-class Edge {
-    int to, dist;
+class Point {
+    int r, c;
+    String alphabet;
 
-    public Edge(int to, int dist) {
-        this.to = to;
-        this.dist = dist;
+    public Point(int r, int c) {
+        this.r = r;
+        this.c = c;
+        alphabet = "";
     }
 }
