350890f1c0a4108ee3dfc279498e5c267b1a5179
Jo YoHan <37216082+slowhigh@users.noreply.github.com>
2025-04-09 17:11:01 +0900

    푸는 중: https://www.acmicpc.net/problem/1107

--- Main.java
+++ Main.java
@@ -1,49 +1,78 @@
 import java.io.*;
 
 class Main {
+    private static boolean[] brokenBtnArr;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String chen = "80000";
-        String[] chenArr = chen.split("");
-        int minPress = Math.abs(Integer.parseInt(chen) - 100);
-        int chenLen = chenArr.length;
-        boolean[] brokenBtnArr = new boolean[10];
-        int brokenCount = Integer.parseInt("2");
-        if (brokenCount >= 10) {
-            System.out.println(minPress);
-            return;
-        } else if (brokenCount == 0) {
-            System.out.println(chenLen);
-            return;
-        }
-        for (String btn : "8 9".split(" ")) {
-            brokenBtnArr[Integer.parseInt(btn)] = true;
+        String chenStr = br.readLine();
+        int chenNum = Integer.parseInt(chenStr);
+        int minPress = Math.abs(chenNum - 100);
+        brokenBtnArr = new boolean[10];
+        int brokenCount = Integer.parseInt(br.readLine());
+        if (brokenCount == 0) {
+            minPress = Math.min(minPress, chenStr.length());
+        } else {
+            for (String btn : br.readLine().split(" ")) {
+                brokenBtnArr[Integer.parseInt(btn)] = true;
+            }
         }
 
-        int btn;
-        int totalPress = chenLen;
-        for (int i = 0; i < chenLen; i++) {
-            btn = Integer.parseInt(chenArr[chenLen - i - 1]);
+        int totalPress = Integer.MAX_VALUE;
+
+        if (brokenCount < 10) {
             int press = 0;
+            int upChen, downChen, count;
+            while (true) {
+                upChen = chenNum + press;
+                downChen = chenNum - press;
 
-            while ((btn - press) >= 0 || (btn + press) <= 9) {
-                if ((btn + press) <= 9 && !brokenBtnArr[btn + press])
+                if (downChen < 0 && upChen > 999999)
                     break;
-                
-                if ((btn - press) >= 0 && !brokenBtnArr[btn - press])
+
+                if (downChen >= 0) {
+                    count = check(downChen);
+                    if (count != -1) {
+                        totalPress = Math.min(totalPress, chenNum - downChen + count);
+                        break;
+                    }
+                }
+
+                if (upChen <= 999999) {
+                    count = check(upChen);
+                    if (count != -1) {
+                        totalPress = Math.min(totalPress, upChen - chenNum + count);
+                        break;
+                    }
+                }
+
+                if (totalPress != Integer.MAX_VALUE && minPress < totalPress) {
                     break;
+                }
 
                 press++;
             }
-
-            totalPress += Math.pow(10, i) * press;
-            if (minPress < totalPress) {
-                System.out.println(minPress);
-                return;
-            }
         }
 
-        System.out.println(totalPress != 0 ? Math.min(minPress, totalPress) : minPress);
+        System.out.println(totalPress != Integer.MAX_VALUE ? Math.min(minPress, totalPress) : minPress);
+    }
+
+    private static int check(int chenNum) {
+        if (chenNum == 0) {
+            return brokenBtnArr[0] ? -1 : 1;
+        }
+
+        int count = 0;
+
+        while (chenNum != 0) {
+            if (brokenBtnArr[chenNum % 10])
+                return -1;
+
+            chenNum /= 10;
+            count++;
+        }
+
+        return count;
     }
 }
