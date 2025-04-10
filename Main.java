28a298468e2e67db2aebabb6b659a63a5ed262be
Jo YoHan <37216082+slowhigh@users.noreply.github.com>
2025-04-10 20:22:19 +0900

    푸는 중: https://www.acmicpc.net/problem/14502

--- Main.java
+++ Main.java
@@ -4,29 +4,34 @@ class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] l1 = br.readLine().split(" ");
-        int N = Integer.parseInt(l1[0]);
-        int S = Integer.parseInt(l1[1]);
-        int min = Integer.MAX_VALUE;
-        int[] arr = new int[N];
-        String[] l2 = br.readLine().split(" ");
-        for (int i = 0; i < N; i++)
-            arr[i] = Integer.parseInt(l2[i]);
-        int left = 0, sum = 0;
-        for (int right = 0; right < N; right++) {
-            sum += arr[right];
+        // 0 0 0 0 X 2
+        // 1 0 0 X 2 2
+        // 1 1 1 2 2 2
+        // 0 0 0 X 2 2
 
-            while (sum >= S) {
-                min = Math.min(min, right - left + 1);
-                sum -= arr[left];
-                left++;
-            }
-        }
+        // - 방문 비트 마스킹
+        // - if 방문 == 0
+        // - if X <= 3
+        //     - 0 -> X
+        // - else
+        //     - 0 -> 2
+        // - if 방문 == X
+        // - if X > 3
+        //     - X -> 2
+        // - else 
+        //     - no action
 
-        System.out.println(min != Integer.MAX_VALUE ? min : 0);
+        // for 방문 기준으로 상하좌우
+        // - 신규 in rect
+        // - 2(바이러스), 1(벽)은 안됨
+        // - 방문 유무 확인(이거 조건 생각해봐야함)
+        // - 재귀호출
+
+        // - 재귀호출할 때가 없다?
+        // max = max vs (all - 1 - X(무조건 놔야 하니까 고정 3) - 2)
+
+
+
+        
     }
 }
-// 10 15
-// 5 1 3 5 10 7 4 9 2 8
-
-// 2
