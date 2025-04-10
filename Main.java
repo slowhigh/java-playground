758095373d3b03c1530abff1202bda7f61979588
Jo YoHan <37216082+slowhigh@users.noreply.github.com>
2025-04-10 18:51:20 +0900

    https://www.acmicpc.net/problem/1806 제출

--- Main.java
+++ Main.java
@@ -1,73 +1,32 @@
 import java.io.*;
 
 class Main {
-    private static boolean[] brokenBtnArr;
-    private static final int MAX_CHANNEL = 999999;
-    private static final int DEFAULT_CHANNEL = 100;
-    private static int minPress = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int targetChannel = Integer.parseInt(br.readLine());
-        minPress = Math.abs(targetChannel - DEFAULT_CHANNEL);
+        String[] l1 = br.readLine().split(" ");
+        int N = Integer.parseInt(l1[0]);
+        int S = Integer.parseInt(l1[1]);
+        int min = Integer.MAX_VALUE;
+        int[] arr = new int[N];
+        String[] l2 = br.readLine().split(" ");
+        for (int i = 0; i < N; i++)
+            arr[i] = Integer.parseInt(l2[i]);
+        int left = 0, sum = 0;
+        for (int right = 0; right < N; right++) {
+            sum += arr[right];
 
-        int brokenCount = Integer.parseInt(br.readLine());
-        if (brokenCount == 0) {
-            minPress = Math.min(minPress, String.valueOf(targetChannel).length());
-        } else {
-            brokenBtnArr = new boolean[10];
-            for (String btn : br.readLine().split(" ")) {
-                brokenBtnArr[Integer.parseInt(btn)] = true;
-            }
-            if (brokenCount < 10) {
-                minPress = Math.min(minPress, findMinPress(targetChannel));
+            while (sum >= S) {
+                min = Math.min(min, right - left + 1);
+                sum -= arr[left];
+                left++;
             }
         }
 
-        System.out.println(minPress);
-    }
-
-    private static int findMinPress(int targetChannel) {
-        int minPress = Integer.MAX_VALUE;
-        for (int press = 0;; press++) {
-            int upChannel = targetChannel + press;
-            int downChannel = targetChannel - press;
-
-            if (downChannel < 0 && upChannel > MAX_CHANNEL)
-                break;
-
-            if (downChannel >= 0) {
-                int count = getDigitCount(downChannel);
-                if (count > 0) {
-                    return press + count;
-                }
-            }
-
-            if (upChannel <= MAX_CHANNEL) {
-                int count = getDigitCount(upChannel);
-                if (count > 0) {
-                    return press + count;
-                }
-            }
-
-            if (minPress < press)
-                break;
-        }
-
-        return minPress;
-    }
-
-    private static int getDigitCount(int number) {
-        if (number == 0)
-            return brokenBtnArr[0] ? -1 : 1;
-
-        int count = 0;
-        while (number > 0) {
-            if (brokenBtnArr[number % 10])
-                return -1;
-            number /= 10;
-            count++;
-        }
-        return count;
+        System.out.println(min != Integer.MAX_VALUE ? min : 0);
     }
 }
+// 10 15
+// 5 1 3 5 10 7 4 9 2 8
+
+// 2
