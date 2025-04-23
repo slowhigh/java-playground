4ae2a3f2b37a3551154b6d77e712ef546331ef11
Jo YoHan <37216082+slowhigh@users.noreply.github.com>
2025-04-23 20:29:15 +0900

    푸는 중: https://www.acmicpc.net/problem/14502

--- Main.java
+++ Main.java
@@ -1,107 +1,94 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    private static final int[] DR = { -1, 1, 0, 0 }, DC = { 0, 0, -1, 1 };
-    private static int _n, _m, _wall;
-    private static Set<Long> _visited;
+// 문제
+// 인체에 치명적인 바이러스를 연구하던 연구소에서 바이러스가 유출되었다. 다행히 바이러스는 아직 퍼지지 않았고, 바이러스의 확산을 막기 위해서 연구소에 벽을 세우려고 한다.
 
+// 연구소는 크기가 N×M인 직사각형으로 나타낼 수 있으며, 직사각형은 1×1 크기의 정사각형으로 나누어져 있다. 연구소는 빈 칸, 벽으로 이루어져 있으며, 벽은 칸 하나를 가득 차지한다. 
+
+// 일부 칸은 바이러스가 존재하며, 이 바이러스는 상하좌우로 인접한 빈 칸으로 모두 퍼져나갈 수 있다. 새로 세울 수 있는 벽의 개수는 3개이며, 꼭 3개를 세워야 한다.
+
+// 예를 들어, 아래와 같이 연구소가 생긴 경우를 살펴보자.
+
+// 2 0 0 0 1 1 0
+// 0 0 1 0 1 2 0
+// 0 1 1 0 1 0 0
+// 0 1 0 0 0 0 0
+// 0 0 0 0 0 1 1
+// 0 1 0 0 0 0 0
+// 0 1 0 0 0 0 0
+// 이때, 0은 빈 칸, 1은 벽, 2는 바이러스가 있는 곳이다. 아무런 벽을 세우지 않는다면, 바이러스는 모든 빈 칸으로 퍼져나갈 수 있다.
+
+// 2행 1열, 1행 2열, 4행 6열에 벽을 세운다면 지도의 모양은 아래와 같아지게 된다.
+
+// 2 1 0 0 1 1 0
+// 1 0 1 0 1 2 0
+// 0 1 1 0 1 0 0
+// 0 1 0 0 0 1 0
+// 0 0 0 0 0 1 1
+// 0 1 0 0 0 0 0
+// 0 1 0 0 0 0 0
+// 바이러스가 퍼진 뒤의 모습은 아래와 같아진다.
+
+// 2 1 0 0 1 1 2
+// 1 0 1 0 1 2 2
+// 0 1 1 0 1 2 2
+// 0 1 0 0 0 1 2
+// 0 0 0 0 0 1 1
+// 0 1 0 0 0 0 0
+// 0 1 0 0 0 0 0
+// 벽을 3개 세운 뒤, 바이러스가 퍼질 수 없는 곳을 안전 영역이라고 한다. 위의 지도에서 안전 영역의 크기는 27이다.
+
+// 연구소의 지도가 주어졌을 때 얻을 수 있는 안전 영역 크기의 최댓값을 구하는 프로그램을 작성하시오.
+
+// 입력
+// 첫째 줄에 지도의 세로 크기 N과 가로 크기 M이 주어진다. (3 ≤ N, M ≤ 8)
+
+// 둘째 줄부터 N개의 줄에 지도의 모양이 주어진다. 0은 빈 칸, 1은 벽, 2는 바이러스가 있는 위치이다. 2의 개수는 2보다 크거나 같고, 10보다 작거나 같은 자연수이다.
+
+// 빈 칸의 개수는 3개 이상이다.
+
+// 출력
+// 첫째 줄에 얻을 수 있는 안전 영역의 최대 크기를 출력한다.
+
+// 4 6
+// 0 0 0 0 0 0
+// 1 0 0 0 0 2
+// 1 1 1 0 0 2
+// 0 0 0 0 0 2
+
+class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] l1 = br.readLine().split(" ");
-
-        _n = Integer.parseInt(l1[0]);
-        _m = Integer.parseInt(l1[1]);
-        _wall = 0;
-        int virus = 0;
-        int[][] grid = new int[_n][_m];
-        _visited = new HashSet<>();
-        Queue<Point> q = new LinkedList<>();
-        String[] ln;
-        for (int i = 0; i < _n; i++) {
-            ln = br.readLine().split(" ");
-            for (int j = 0; j < _m; j++) {
-                grid[i][j] = Integer.parseInt(ln[j]);
-                if (grid[i][j] == 1) {
-                    _wall++;
-                } else if (grid[i][j] == 2) {
-                    virus++;
-                    q.add(new Point(i, j));
+        String[] nm = br.readLine().split(" ");
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+        long wall = -1L;
+        long virus = 0L;
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j += 2) {
+                char c = s.charAt(j);
+                long bit = 1L << (8 * i) + (j / 2);
+                if (c == '0') {
+                    wall ^= bit;
+                } else if (c == '2') {
+                    wall ^= bit;
+                    virus |= bit;
                 }
             }
         }
-        System.out.println(dfs(q, grid, 0, virus));
+        // 3중 for문으로 3개의 벽을 세울 수 있는 모든 상황을 돌면서 바이러스를 dfs로 전파해서 max 개수 세기
 
-        // 첫째 줄에 지도의 세로 크기 N과 가로 크기 M이 주어진다. (3 ≤ N, M ≤ 8)
-        // 4 6
-        // 0 0 0 0 0 0
-        // 1 0 0 0 0 2
-        // 1 1 1 0 0 2
-        // 0 0 0 0 0 2
-        // 9
-
-        // oldWall, ,
-
-        // - 방문 비트 마스킹
-        // - if 방문 == 0
-        // - if X <= 3
-        // - 0 -> X
-        // - else
-        // - 0 -> 2
-        // - if 방문 == X
-        // - if X > 3
-        // - X -> 2
-        // - else
-        // - no action
-
-        // for 방문 기준으로 상하좌우
-        // - 신규 in rect
-        // - 2(바이러스), 1(벽)은 안됨
-        // - 방문 유무 확인(이거 조건 생각해봐야함)
-        // - 재귀호출
-
-        // - 재귀호출할 때가 없다?
-        // max = max vs (all - 1 - X(무조건 놔야 하니까 고정 3) - 2)
-    }
-
-    private static int dfs(Queue<Point> q, int[][] grid, int wall, int virus) {
-        // visited = visited << (p.row * m + p.col);
-        // gVisited.add(visited);
-
-        if (q.size() == 0)
-            return (_n * _m) - _wall - 3 - virus;
-
-        Point p = q.poll();
-        if (grid[p.row][p.col] == 0) {
-            grid[p.row][p.col] = wall <= 3 ? -1 : 2;
-        } else { // -1
-            if (wall > 3)
-                grid[p.row][p.col] = 2;
-        }
-
-        int max = 0;
-
-        for (int i = 0; i < 4; i++) {
-            int nr = p.row + DR[i], nc = p.col + DC[i];
-
-            if (nr < 0 || nc < 0 || nr >= _n || nc >= _m)
-                continue;
-
-            if (grid[nr][nc] == 2 || grid[nr][nc] == 1)
-                continue;
-
-            max = Math.max(max, dfs(new LinkedList<>(q), grid, wall, virus));
-        }
-
-        return max;
-    }
-}
-
-class Point {
-    int row, col;
-
-    public Point(int row, int col) {
-        this.row = row;
-        this.col = col;
+        System.out.printf("%"Long.toBinaryString(wall));
+        // 1 1 1 1 1 1 1 1
+        // 1 1 1 1 1 1 1 1
+        // 1 1 1 1 1 1 1 1
+        // 1 - - - - - - -
+        // 1 | 1 1 1 0 0 0
+        // 1 | 1 1 1 1 1 1
+        // 1 | 1 1 1 0 0 1
+        // 1 | 1 1 1 0 0 0
+        System.out.println(Long.toBinaryString(virus));
     }
 }
