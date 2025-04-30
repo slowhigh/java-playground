c11d3f0a9b5e5908751d0aafcec3a9489c1ca733
Jo YoHan <37216082+slowhigh@users.noreply.github.com>
2025-04-30 15:45:10 +0900

    https://www.acmicpc.net/problem/2580 제출

--- Main.java
+++ Main.java
@@ -2,74 +2,79 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    private static int n, m;
+    private static final int S = 9;
+    private static final int BLANK = 0;
+    private static int[][] grid;
+    private static boolean[][] rowCheck, colCheck, boxCheck;
+    private static List<int[]> blanks;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] nm = br.readLine().split(" ");
-        n = Integer.parseInt(nm[0]);
-        m = Integer.parseInt(nm[1]);
-        long wallMap = 0L;
-        long virusMap = 0L;
-        int maxSafeZone = 0;
-        List<Integer> virus = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            String s = br.readLine();
 
-            for (int j = 0; j < m; j++) {
-                char c = s.charAt(j * 2);
-                int idx = (m * i) + j;
-                if (c == '1') {
-                    wallMap |= 1L << idx;
-                } else if (c == '2') {
-                    virusMap |= 1L << idx;
-                    virus.add(idx);
+        grid = new int[S][S];
+        rowCheck = new boolean[S][S + 1];
+        colCheck = new boolean[S][S + 1];
+        boxCheck = new boolean[S][S + 1];
+        blanks = new ArrayList<>();
+
+        for (int i = 0; i < S; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < S; j++) {
+                int num = Character.getNumericValue(s.charAt(j * 2));
+                grid[i][j] = num;
+                if (num == BLANK) {
+                    blanks.add(new int[] { i, j });
+                    continue;
                 }
+
+                rowCheck[i][num] = true;
+                colCheck[j][num] = true;
+                boxCheck[getBoxIdx(i, j)][num] = true;
             }
         }
 
-        long newWallMap, newVirusMap;
-        for (int i = 0; i < n * m - 2; i++) {
-            if (checkMap(i, wallMap, virusMap))
+        check(0);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < S; i++) {
+            for (int j = 0; j < S; j++) {
+                sb.append(grid[i][j]);
+                sb.append((j < S - 1) ? " " : "\n");
+            }
+        }
+        System.out.print(sb.toString());
+    }
+
+    private static boolean check(int idx) {
+        if (idx == blanks.size())
+            return true;
+
+        int[] blank = blanks.get(idx);
+        int row = blank[0];
+        int col = blank[1];
+
+        for (int num = 1; num <= S; num++) {
+            if (rowCheck[row][num] || colCheck[col][num] || boxCheck[getBoxIdx(row, col)][num])
                 continue;
 
-            for (int j = i + 1; j < (n * m) - 1; j++) {
-                if (checkMap(j, wallMap, virusMap))
-                    continue;
+            grid[row][col] = num;
+            rowCheck[row][num] = true;
+            colCheck[col][num] = true;
+            boxCheck[getBoxIdx(row, col)][num] = true;
 
-                for (int k = j + 1; k < (n * m); k++) {
-                    if (checkMap(k, wallMap, virusMap))
-                        continue;
+            if (check(idx + 1))
+                return true;
 
-                    newWallMap = wallMap | 1L << i | 1L << j | 1L << k;
-                    newVirusMap = virusMap;
-
-                    for (int v : virus)
-                        newVirusMap = spread(v, newWallMap, newVirusMap);
-
-                    maxSafeZone = Math.max(maxSafeZone,
-                            (m * n) - Long.bitCount(newWallMap) - Long.bitCount(newVirusMap));
-                }
-            }
+            grid[row][col] = BLANK;
+            rowCheck[row][num] = false;
+            colCheck[col][num] = false;
+            boxCheck[getBoxIdx(row, col)][num] = false;
         }
 
-        System.out.println(maxSafeZone);
+        return false;
     }
 
-    private static long spread(int i, long wallMap, long virusMap) {
-        if (i - m >= 0 && !checkMap(i - m, wallMap, virusMap))
-            virusMap = spread(i - m, wallMap, virusMap | (1L << (i - m)));
-        if (i + m < m * n && !checkMap(i + m, wallMap, virusMap))
-            virusMap = spread(i + m, wallMap, virusMap | (1L << (i + m)));
-        if (i % m > 0 && !checkMap(i - 1, wallMap, virusMap))
-            virusMap = spread(i - 1, wallMap, virusMap | (1L << (i - 1)));
-        if ((i + 1) % m != 0 && !checkMap(i + 1, wallMap, virusMap))
-            virusMap = spread(i + 1, wallMap, virusMap | (1L << (i + 1)));
-
-        return virusMap;
-    }
-
-    private static boolean checkMap(int i, long wallMap, long virusMap) {
-        return ((wallMap & (1L << i)) == (1L << i)) || ((virusMap & (1L << i)) == (1L << i));
+    private static int getBoxIdx(int row, int col) {
+        return ((row / 3) * 3) + (col / 3);
     }
 }
