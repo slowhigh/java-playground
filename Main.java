98716a2c69d082dd909077b359dab2feb86a0484
Jo YoHan <37216082+slowhigh@users.noreply.github.com>
2025-05-07 19:49:46 +0900

    https://www.acmicpc.net/problem/1707 제출

--- Main.java
+++ Main.java
@@ -1,72 +1,64 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-    private static final int S = 9;
-    private static final int BOX_SIZE = 3;
-    private static final int BLANK = 0;
-    private static int[][] grid;
-    private static boolean[][] rowCheck, colCheck, boxCheck;
-    private static List<int[]> blanks;
+public class Main {
+    private static List<Integer>[] graph;
+    private static int[] colors;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-        grid = new int[S][S];
-        rowCheck = new boolean[S][S + 1];
-        colCheck = new boolean[S][S + 1];
-        boxCheck = new boolean[S][S + 1];
-        blanks = new ArrayList<>();
+        for (int k = 0; k < K; k++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int V = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < S; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < S; j++) {
-                int num = Character.getNumericValue(s.charAt(j * 2));
-                grid[i][j] = num;
-                if (num == BLANK) {
-                    blanks.add(new int[] { i, j });
-                } else {
-                    rowCheck[i][num] = colCheck[j][num] = boxCheck[getBoxIdx(i, j)][num] = true;
+            graph = new List[V + 1];
+            for (int i = 1; i <= V; i++)
+                graph[i] = new ArrayList<>();
+
+            for (int i = 0; i < E; i++) {
+                st = new StringTokenizer(br.readLine());
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                graph[u].add(v);
+                graph[v].add(u);
+            }
+
+            colors = new int[V + 1]; // 0: 미방문, 1 or -1: 그룹 색상
+            boolean result = true;
+
+            for (int i = 1; i <= V; i++) {
+                if (colors[i] != 0)
+                    continue;
+
+                if (!dfsCheck(i, 1)) {
+                    result = false;
+                    break;
                 }
+            }
+
+            sb.append(result ? "YES\n" : "NO\n");
+        }
+
+        System.out.print(sb);
+    }
+
+    private static boolean dfsCheck(int node, int color) {
+        colors[node] = color;
+
+        for (int neighbor : graph[node]) {
+            if (colors[neighbor] == 0) {
+                if (!dfsCheck(neighbor, -color)) {
+                    return false;
+                }
+            } else if (colors[neighbor] == colors[node]) {
+                return false;
             }
         }
 
-        check(0);
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < S; i++)
-            for (int j = 0; j < S; j++)
-                sb.append(grid[i][j]).append((j < S - 1) ? " " : "\n");
-
-        System.out.print(sb.toString());
-    }
-
-    private static boolean check(int idx) {
-        if (idx == blanks.size())
-            return true;
-
-        int[] blank = blanks.get(idx);
-        int row = blank[0], col = blank[1];
-        int box = getBoxIdx(row, col);
-
-        for (int num = 1; num <= S; num++) {
-            if (rowCheck[row][num] || colCheck[col][num] || boxCheck[box][num])
-                continue;
-
-            grid[row][col] = num;
-            rowCheck[row][num] = colCheck[col][num] = boxCheck[box][num] = true;
-
-            if (check(idx + 1))
-                return true;
-
-            grid[row][col] = BLANK;
-            rowCheck[row][num] = colCheck[col][num] = boxCheck[box][num] = false;
-        }
-
-        return false;
-    }
-
-    private static int getBoxIdx(int row, int col) {
-        return ((row / BOX_SIZE) * BOX_SIZE) + (col / BOX_SIZE);
+        return true;
     }
 }
